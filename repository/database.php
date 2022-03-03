57bcc7f31305bb0412af513baa5687e10a87418a
Rober <suarezgarciaroberto@hotmail.com>
2022-03-03 21:21:46 +0000

    fix db

--- repository/database.php
+++ repository/database.php
@@ -1,27 +1,33 @@
 <?php 
 class Database {
   
-  private $database = null;
-  private $hostname = null;
-  private $username = null;
-  private $password = null;
+  // private $database = null;
+  // private $hostname = null;
+  // private $username = null;
+  // private $password = null;
   
   public $conn;
 
-  function __construct() {
+  // function __construct() {
+  //   $url = getenv('JAWSDB_URL');
+  //   $dbparts = parse_url($url);
+  //   $hostname = $dbparts['host'];
+  //   $username = $dbparts['user'];
+  //   $password = $dbparts['pass'];
+  //   $database = ltrim($dbparts['path'],'/');
+  // }
+  
+  public function getConnection(){
+    $this->conn = null;
     $url = getenv('JAWSDB_URL');
     $dbparts = parse_url($url);
     $hostname = $dbparts['host'];
     $username = $dbparts['user'];
     $password = $dbparts['pass'];
     $database = ltrim($dbparts['path'],'/');
-  }
-  
-  public function getConnection(){
-    $this->conn = null;
-    echo $this->username;
-    $this->conn = new mysqli($this->hostname,$this->username,$this->password,$this->database);
-    if($this->conn->connect_errno){
+    $this->conn = new mysqli($hostname,$username,$password,$database);
+    if($this->conn->connect_error){
+      echo getenv('JAWSDB_URL');
       echo "An error occurred while trying to connect to the database.";
     }
     return $this->conn;
