cdfb6c14138a3b7b00c0f6bb333951f441ca7162
Rober <suarezgarciaroberto@hotmail.com>
2022-03-03 21:15:08 +0000

    reset db script

--- repository/database.php
+++ repository/database.php
@@ -1,23 +1,25 @@
 <?php 
 class Database {
-  // JAWS_DB
-  private $host = "spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-  private $user = "mnocy6yjwekgtlh6";
-  private $pass = "sf8qj292v0ayafbu";
-  private $db = "hqc4r8e6uym9fwa6";
   
-  // CLEAR_DB
-  // private $host = "eu-cdbr-west-02.cleardb.net";
-  // private $user = "b2ef7e7e5364f6";
-  // private $pass = "927608cc";
-  // private $db = "heroku_33ec3d5d2fffe7e";
-  
+  private $database = null;
+  private $hostname = null;
+  private $username = null;
+  private $password = null;
   
   public $conn;
 
+  function __construct() {
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+    $hostname = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $database = ltrim($dbparts['path'],'/');
+  }
+  
   public function getConnection(){
     $this->conn = null;
-    $this->conn = new mysqli($this->host,$this->user,$this->pass,$this->db);
+    $this->conn = new mysqli($this->hostname,$this->username,$this->password,$this->database);
     if($this->conn->connect_errno){
       echo "An error occurred while trying to connect to the database.";
     }
