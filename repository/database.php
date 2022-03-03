45bb59182c25f3f222e18cac0046c77b4ada86d1
Rober <suarezgarciaroberto@hotmail.com>
2022-03-03 17:47:13 +0000

    changed db

--- repository/database.php
+++ repository/database.php
@@ -1,16 +1,16 @@
 <?php 
 class Database {
   // JAWS_DB
-  // private $host = "spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
-  // private $user = "mnocy6yjwekgtlh6";
-  // private $pass = "sf8qj292v0ayafbu";
-  // private $db = "hqc4r8e6uym9fwa6";
+  private $host = "spryrr1myu6oalwl.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+  private $user = "mnocy6yjwekgtlh6";
+  private $pass = "sf8qj292v0ayafbu";
+  private $db = "hqc4r8e6uym9fwa6";
   
   // CLEAR_DB
-  private $host = "eu-cdbr-west-02.cleardb.net";
-  private $user = "b2ef7e7e5364f6";
-  private $pass = "927608cc";
-  private $db = "heroku_33ec3d5d2fffe7e";
+  // private $host = "eu-cdbr-west-02.cleardb.net";
+  // private $user = "b2ef7e7e5364f6";
+  // private $pass = "927608cc";
+  // private $db = "heroku_33ec3d5d2fffe7e";
   
   
   public $conn;
